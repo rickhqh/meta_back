b17b45cc82f3e808d9623735ccd4153acf4a6e0c
rick <rickhqh@foxmail.com>
2022-12-16 12:55:08 +0800

    fix : 添加日程时间

--- meta/src/main/java/com/ruoyi/meta/domain/Schedule.java
+++ meta/src/main/java/com/ruoyi/meta/domain/Schedule.java
@@ -1,9 +1,11 @@
 package com.ruoyi.meta.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import lombok.Data;
+
+import java.util.Date;
 
 /**
  * 日程对象 mt_schedule
@@ -11,6 +13,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author rick
  * @date 2022-12-12
  */
+@Data
 public class Schedule extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
@@ -24,30 +27,7 @@ public class Schedule extends BaseEntity {
      */
     @Excel(name = "日程内容")
     private String scheduleContent;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date scheduleTime;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getScheduleContent() {
-        return scheduleContent;
-    }
-
-    public void setScheduleContent(String scheduleContent) {
-        this.scheduleContent = scheduleContent;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-                .append("id", getId())
-                .append("scheduleContent", getScheduleContent())
-                .append("createTime", getCreateTime())
-                .append("updateTime", getUpdateTime())
-                .toString();
-    }
 }
