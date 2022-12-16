cc153c86f1bc751f363d1e4a9812cf3c255e2751
rick <rickhqh@foxmail.com>
2022-12-16 13:15:21 +0800

    fix : 添加日程时间

--- meta/src/main/java/com/ruoyi/meta/domain/Schedule.java
+++ meta/src/main/java/com/ruoyi/meta/domain/Schedule.java
@@ -1,11 +1,8 @@
 package com.ruoyi.meta.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * 日程对象 mt_schedule
@@ -27,7 +24,6 @@ public class Schedule extends BaseEntity {
      */
     @Excel(name = "日程内容")
     private String scheduleContent;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date scheduleTime;
+    private String scheduleTime;
 
 }
